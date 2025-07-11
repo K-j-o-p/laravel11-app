55a208491e59c2e2856b68d2d57bbbbe0644297a
KUSI JOEL OSEI POKU <jkusi158@gmail.com>
2025-07-11 12:26:55 +0000

    data test 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 <x-layout>
 
- 
+  Home
+
+ @foreach ($men as $man)
+ <div>
+    <h1>NAME:{{$man['name']}}</h1>
+    <h1>NAME:{{$man['age']}}</h1>
+ </div>
+     
+ @endforeach
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home' ,["men"=>"[
+    'name'=>'joel',
+    'age'=> 20]"]);
 });
 
 Route::get('/properties', function () {
