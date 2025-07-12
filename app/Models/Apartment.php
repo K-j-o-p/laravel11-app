b6b8608491ec770ad3b93ca143fe8d508307a25d
KUSI JOEL OSEI POKU <jkusi158@gmail.com>
2025-07-12 15:18:06 +0000

    live from data base

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -1,39 +1,14 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Apartment{
-    public static function all():array{ 
-
-        return [
-            [    
-                'id'=> 1,
-                'name'=>'Apartment 1',
-                'location'=>'Tema',
-                'price'=> 100,
-                'description'=>'A beautiful apartment in Tema with morden amenities.'
-            ],
-
-            [
-                'id'=> 2,
-                'name'=>'Apartment 2',
-                'location'=>'Tema',
-                'price'=> 100,
-                'description'=>'A beautiful apartment in Tema with morden amenities.'
-            ],
-        ];
-    }
-
-    public static  function find(int $id) :array{
-
-          $apartment= Arr::first(static::all(), fn($apartment)=> $apartment['id']== $id);
-
-             if(!$apartment){
-                abort(404);
-             }  
-             return $apartment;
-
-    }
+class Apartment extends Model{
+    use HasFactory;
+    
+   protected $fillable=['name','price','location','description'];
     
 }
---
+++ database/factories/ApartmentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Apartment>
+ */
+class ApartmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=>fake()->name(),
+            'price'=>'$10,000',
+            'location'=>fake()->city(),
+            'description'=>fake()->paragraph()
+            
+        ];
+    }
+}
